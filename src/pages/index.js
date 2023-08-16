03dec3023d75fc10bb1f1bf47b739d051ee17606
sihwadang <sbird96@msn.com>
2023-08-16 14:32:38 +0900

    v 0,1,0

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main}`}>
-        <h2>시화당 웹서비스</h2>
+        <h2>시화당 웹서비스 테스트</h2>
 
       </main>
     </>
